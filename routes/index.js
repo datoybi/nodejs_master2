6bb12dde0cd5a1cc506b1181c7922405fcb7153f
dasomyun <dsy0302@gmail.com>
2018-09-15 17:42:03 +0900

    Express에 mysql 연동

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var template = require('../lib/template');
+var db = require('../lib/db');
+
 
 router.get('/', function (req, res) {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(req.list);
+db.query('SELECT * FROM topic', function(error, topics){
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(topics);
     var html = template.HTML(title, list,
       `<h2>${title}</h2>${description}
       <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">`,
@@ -13,6 +16,6 @@ router.get('/', function (req, res) {
     );
     res.send(html);
   });
-
+});
   module.exports = router;
   
--- routes/topic.js
+++ routes/topic.js
@@ -4,65 +4,62 @@ var path = require('path');
 var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
+var db = require('../lib/db');
 
 router.get('/create', function(req, res){
-    var title = 'WEB - create';
-    var list = template.list(req.list);
-    var html = template.HTML(title, list, `
-      <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    res.send(html);
+db.query('SELECT * FROM topic', function(error, topics){
+  var title = 'WEB - create';
+  var list = template.list(topics);
+  var html = template.HTML(title, list, `
+    <form action="/topic/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  res.send(html);
   });
+});
   
-  router.post('/create_process', function(req, res){
-    var post = req.body;
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      res.redirect(`/topic/${title}`);
-    });
+router.post('/create_process', function(req, res){
+  var post = req.body;
+  db.query('INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, now(), 1)', [post.title, post.description], function(error, topic){
+    res.redirect(`/topic/${topic.insertId}`);
   });
+});
   
   router.get('/update/:pageId', function(req, res){
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = req.params.pageId;
-      var list = template.list(req.list);
-      var html = template.HTML(title, list,
-        `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-      );
-      res.send(html);
+    db.query('SELECT * FROM topic', function(error1, topics){
+      db.query(`SELECT * FROM topic WHERE id=?`, [req.params.pageId], function(error2, topic){
+        var list = template.list(topics);
+        var html = template.HTML(topic[0].title, list,
+          `
+          <form action="/topic/update_process" method="post">
+            <input type="hidden" name="id" value="${topic[0].title}">
+            <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+          `<a href="/topic/create">create</a> <a href="/topic/update/${topic[0].id}">update</a>`
+        );
+        res.send(html);
+      });
     });
   });
+
   router.post('/update_process', function(req, res){
     var post = req.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        res.redirect(`/topic/${title}`);
-      })
+    db.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?', [post.title, post.description, post.id], function(error, result){
+    res.redirect(`/topic/${result.insertId}`);
     });
   });
   
@@ -76,30 +73,24 @@ router.get('/create', function(req, res){
   });
   
   router.get('/:pageId/', function (req, res, next) {
-    fs.readdir('./data', function(error, filelist){
-      var filteredId = path.parse(req.params.pageId).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-        if(err) {
-          next(err);  // error 
-        } else {
-        var title = req.params.pageId;
+    db.query('SELECT * FROM topic', function(error1, topics){
+      db.query(`SELECT * FROM topic WHERE id=?`, [req.params.pageId], function(error2, topic){
+        var title = topic[0].title;
+        console.log(title);
         var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-        var list = template.list(filelist);
+        var sanitizedDescription = sanitizeHtml(topic[0].description);
+        var list = template.list(topics);
         var html = template.HTML(sanitizedTitle, list,
           `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
           ` <a href="/topic/create">create</a>
-            <a href="/topic/update/${sanitizedTitle}">update</a>
+            <a href="/topic/update/${topic[0].id}">update</a>
             <form action="/topic/delete_process" method="post">
               <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
             </form>`
-        );
-        res.send(html);
-      } 
-     });
+          );
+          res.send(html);
+      });
     });
   });
   module.exports = router;
